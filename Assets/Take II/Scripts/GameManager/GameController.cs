3ef006ffbaf83935140e469c8f55027696abe905
David Chavarria <dvchavarriam@gmail.com>
2018-09-24 05:31:05 -0700

    GameController sort by agility

--- Assets/Take II/Scripts/GameManager/GameController.cs
+++ Assets/Take II/Scripts/GameManager/GameController.cs
@@ -1,18 +1,37 @@
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Take_II.Scripts.EnemyManager;
 using Assets.Take_II.Scripts.PlayerManager;
 using UnityEngine;
 
 namespace Assets.Take_II.Scripts.GameManager
- {
-     public class GameController: MonoBehaviour {
-         public static GameController Manager { get; private set; }
-         public List<Player> Players = new List<Player>();
-         public List<Enemy> Enemies = new List<Enemy>();
-        
-         void Awake()
-         {
-             Manager = this;
-         }
-     }
- }
+{
+    public class GameController : MonoBehaviour
+    {
+
+        public static GameController Manager { get; private set; }
+
+        public List<Player> Players
+        {
+            get { return _players; }
+            set { _players = new List<Player>(value.OrderByDescending(p => p.Stats.Agility)); }
+        }
+
+        public List<Enemy> Enemies
+        {
+            get { return _enemies; }
+            set { _enemies = new List<Enemy>(value.OrderByDescending(p => p.Stats.Agility)); }
+        }
+
+        [SerializeField]
+        private List<Player> _players = new List<Player>();
+
+        [SerializeField]
+        private List<Enemy> _enemies = new List<Enemy>();
+
+        void Awake()
+        {
+            Manager = this;
+        }
+    }
+}
