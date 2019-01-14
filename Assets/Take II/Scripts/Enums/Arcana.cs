d3f0c177ba564314233727db0607659eac3d788f
wawila <davidc@neon.fyi>
2019-01-13 19:32:56 -0800

    Arcana enum

---
+++ Assets/Take II/Scripts/Enums/Arcana.cs
@@ -0,0 +1,15 @@
+namespace Assets.Take_II.Scripts.Enums
+{
+    public enum Arcana
+    {
+        Fool, Magician, Priestess, Empress, 
+        Emperor, Heirophant, Lovers, Chariot, 
+        Justice, Hermit, Fortune, Strength, 
+        HangMan, Death, Temperance, Devil,
+        Tower, Star, Moon, Sun, Judgement  
+    }
+
+    public enum MinorArcana {
+        Cups, Staves, Coins, Swords
+    }
+}
