0a22c149ade3fb4a43f6e62461f1deb43b01959b
wawila <davidc@neon.fyi>
2019-01-28 17:38:25 -0800

    Stat changes
    Makes it easier to change values programatically

--- Assets/Personas/PersonaBase.cs
+++ Assets/Personas/PersonaBase.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Assets.Spells;
 using Assets.Take_II.Scripts.Enums;
@@ -7,27 +8,30 @@ namespace Assets.Personas
     public abstract class PersonaBase {
         public abstract string Name { get; }
         public int Level { get; protected set; }
-        public Arcana Arcana { get; protected set; }
-        public int Strength { get; protected set; }
-        public int Magic { get; protected set; }
-        public int Endurance { get; protected set; }
-        public int Agility { get; protected set; }
-        public int Luck { get; protected set; }
+        public Arcana Arcana { get; }
+        public int Strength => Stats[Statistics.Strength] + StatBuffs.GetValueOrDefault(Statistics.Strength, 0);
+        public int Magic => Stats[Statistics.Magic] + StatBuffs.GetValueOrDefault(Statistics.Magic, 0);
+        public int Endurance => Stats[Statistics.Endurance] + StatBuffs.GetValueOrDefault(Statistics.Endurance, 0);
+        public int Agility => Stats[Statistics.Agility] + StatBuffs.GetValueOrDefault(Statistics.Agility, 0);
+        public int Luck => Stats[Statistics.Luck] + StatBuffs.GetValueOrDefault(Statistics.Luck, 0);
         public StatsModifiers AttackBuff = StatsModifiers.None;
         public StatsModifiers DefenceBuff = StatsModifiers.None;
         public StatsModifiers EvadeBuff = StatsModifiers.None;
         public StatsModifiers HitBuff = StatsModifiers.None;
         public bool MindCharged = false;
         public bool PowerCharged = false;
+        public bool IsPlayerPersona = false;
 
         // TODO: Add buffs for Elemental Attacks; Amp, Boost and accesories
         public SpellBook SpellBook { get; protected set; }
         public Dictionary<Elements, ResistanceModifiers> Resistances { get; protected set; }
-        public bool IsPlayerPersona = false;
 
+        protected Dictionary<Statistics, int> Stats;
+        protected Dictionary<Statistics, int> StatBuffs;
+    
         public PersonaBase(Arcana arcana, Elements inheritanceElement) {
             Arcana = arcana;
-            SpellBook = new SpellBook(this, inheritanceElement, GetBaseSpellbook());
+            SpellBook = new SpellBook(this, inheritanceElement, GetBaseSpellbook()); 
 
             Resistances = new Dictionary<Elements, ResistanceModifiers>
             {
