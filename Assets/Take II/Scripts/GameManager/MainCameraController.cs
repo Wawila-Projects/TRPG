e772a0ca7e654c3c4ef1f207ed234d585a2e8d6e
wawila <davidc@neon.fyi>
2019-01-30 16:11:17 -0800

    Drag and zoom Camera

---
+++ Assets/Take II/Scripts/GameManager/MainCameraController.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+
+namespace Assets.Take_II.Scripts.GameManager {
+    public class MainCameraController: MonoBehaviour {
+        public Camera MainCamera;
+        
+        Vector3 MouseStart;
+        Vector3 MouseMove;
+        public Bounds CameraBounds;
+
+        public Vector3 NewPosition;
+        public bool IsInside;
+
+        private float CameraZoom => MainCamera.orthographicSize;
+        private Vector3 CameraPosition => MainCamera.transform.position;
+
+        void Awake() {
+            CameraBounds = OrthographicBounds(MainCamera);
+        }
+
+        void LateUpdate() {
+            HandleZoom(Input.GetAxis("Mouse ScrollWheel"));
+            HandleDrag();
+        }
+
+        private void HandleZoom(float scrollMovement) {
+            if (scrollMovement == 0)
+                return;
+
+            var change = CameraZoom + scrollMovement;
+            var newValue = Mathf.Clamp(change, 1, 4);
+
+            MainCamera.orthographicSize = newValue;
+            
+            if (scrollMovement < 0) 
+                return;
+            
+            if (newValue > 3.9) {
+                NewPosition = CameraBounds.center;
+                transform.position = NewPosition;
+                return;
+            }
+            
+            var percentage = CameraZoom / 4f;
+            NewPosition = Vector3.Lerp(CameraPosition, CameraBounds.center, 
+                                percentage / (CameraBounds.center - CameraPosition).magnitude);
+            transform.position = NewPosition;
+        }
+       
+        private void HandleDrag() {
+            if (Input.GetMouseButtonDown (0)) {
+                MouseStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            }
+            else if (Input.GetMouseButton (0)) {
+                MouseMove = new Vector2(Input.mousePosition.x - MouseStart.x, 
+                                        Input.mousePosition.y - MouseStart.y);
+                MouseStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+
+                var newX = transform.position.x + MouseMove.x * Time.deltaTime;
+                var newY = transform.position.y + MouseMove.y * Time.deltaTime;
+                NewPosition = new Vector3(newX, newY, -10);
+
+                IsInside = PointIsInsideBounds(ref NewPosition, CameraBounds);
+
+                if (IsInside) {
+                    transform.position = NewPosition;
+                }
+            }
+        }
+
+        private Bounds OrthographicBounds(Camera camera) {
+            float screenAspect = camera.aspect;
+            float cameraHeight = CameraZoom * 2;
+            return new Bounds( CameraPosition,
+                new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+        }
+
+        private bool PointIsInsideBounds(ref Vector3 point, Bounds bounds) {
+            var extents = OrthographicBounds(MainCamera).extents;
+            var left = point.x - extents.x > bounds.center.x - bounds.extents.x;
+            var right = point.x + extents.x < bounds.center.x + bounds.extents.x;
+            var bottom = point.y - extents.y > bounds.center.y - bounds.extents.y;
+            var top = point.y + extents.y < bounds.center.y + bounds.extents.y;
+
+            if (!left || !right) {
+                point = new Vector3(CameraPosition.x, point.y, -10);
+            }
+            if (!top || !bottom) {
+                point = new Vector3(point.x, CameraPosition.y, -10);
+            }
+            
+            return left || right || bottom || top;
+        }
+    }
+}
