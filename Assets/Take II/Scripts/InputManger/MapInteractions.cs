f6a18515d85706643664dc57e43df226d34152bf
wawila <davidc@neon.fyi>
2019-05-10 04:55:09 -0700

    Refactor MapInteractions

--- Assets/Take II/Scripts/InputManger/MapInteractions.cs
+++ Assets/Take II/Scripts/InputManger/MapInteractions.cs
@@ -1,32 +1,31 @@
 using UnityEngine;
 
-namespace Assets.Take_II.Scripts.InputManger
-{
-    public class MapInteractions : MonoBehaviour
-    {
+namespace Assets.Take_II.Scripts.InputManger {
+    public class MapInteractions : MonoBehaviour {
         public Tile Selected;
 
-        public void DrawReachableArea(int total, Tile selected, bool isRange = false) {
-            ColorReachableArea(total, selected, Color.red, isRange);
+        public void DrawReachableArea (int total, Tile selected, bool isRange = false) {
+            ColorReachableArea (total, selected, Color.green, isRange);
         }
 
-        public void ClearReachableArea(int total, Tile selected, bool isRange = false) {
-            ColorReachableArea(total, selected, Color.white, isRange);
+        public void ClearReachableArea (int total, Tile selected, bool isRange = false) {
+            ColorReachableArea (total, selected, Color.white, isRange);
         }
-        public void ColorReachableArea(int total, Tile selected, Color color, bool isRange)
-        {
-            if (selected == null) 
+        public void ColorReachableArea (int total, Tile selected, Color color, bool isRange) {
+            if (selected == null)
                 return;
 
-            var tiles = selected.GetTilesInsideRange(total);
-            foreach (var tile in tiles)
-            {
-                if (isRange && selected.Neighbors.Contains(tile)) {
-                    continue;
-                }
+            var tiles = selected.GetTilesInsideRange (total);
+            foreach (var tile in tiles) {
+                var renderer = tile.GetComponent<Renderer> ();
+                renderer.material.color = color;
+            }
 
-                var sprite = tile.GetComponentInChildren<SpriteRenderer>();
-                sprite.color = color;
+            var attackRange = isRange ? 2 : 1;
+            tiles = selected.GetTilesAtDistance (total + attackRange);
+            foreach (var tile in tiles) {
+                var renderer = tile.GetComponent<Renderer> ();
+                renderer.material.color = color == Color.white ? Color.white : Color.red;
             }
         }
     }
