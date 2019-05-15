8b5764a3d38083362eac1faf121b61f146eb705f
wawila <davidc@neon.fyi>
2019-05-15 16:16:48 -0600

    Almighty and Physical Lexicon

---
+++ Assets/Spells/SpellLexicon/AlmightyLexicon.cs
@@ -0,0 +1,12 @@
+using Assets.Spells.AlmightySpells;
+
+namespace Assets.Spells.SpellLexicon
+{
+    public class AlmightyLexicon {
+        public Megido Megido => new Megido();
+        public Megidola Megidola => new Megidola();
+        public Megidolaon Megidolaon => new Megidolaon();
+        public BlackViper BlackViper => new BlackViper();
+        public MorningStar MorningStar => new MorningStar();
+    }
+}
---
+++ Assets/Spells/SpellLexicon/PhysicalLexicon.cs
@@ -0,0 +1,14 @@
+using Assets.Spells.PhysicalSpells;
+
+namespace Assets.Spells.SpellLexicon
+{
+    public class PhysicalLexicon {
+        public Bash Bash => new Bash();
+        public Cleave Cleave => new Cleave();
+        public DoubleFangs DoubleFangs => new DoubleFangs();
+        public SingleShot SingleShot => new SingleShot();
+        public Skewer Skewer => new Skewer();
+        public SonicPunch SonicPunch => new SonicPunch();
+
+    }
+}
