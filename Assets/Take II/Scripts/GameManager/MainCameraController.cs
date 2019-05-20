7c0a1a775d687a0561a0678fb806d09194adeb48
wawila <davidc@neon.fyi>
2019-05-20 17:11:42 -0600

    Fixed camera movment
    Reflecting changes from 2D to 3D
    Also made movement smoother

--- Assets/Take II/Scripts/GameManager/MainCameraController.cs
+++ Assets/Take II/Scripts/GameManager/MainCameraController.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Assets.Utils;
 using UnityEngine;
 
 namespace Assets.Take_II.Scripts.GameManager {
@@ -11,33 +13,68 @@ namespace Assets.Take_II.Scripts.GameManager {
         public Character ToTarget;
         private float CameraZoom => MainCamera.orthographicSize;
         private Vector3 CameraPosition => MainCamera.transform.position;
+        public float MaxZoom = 7f;
+        public float ZoomTime;
+        public float ScrollTime;
+        public float ScrollSpeed;
+        [SerializeField]
+        private bool zooming = false;
 
         void Awake() {
             CameraBounds = OrthographicBounds(MainCamera);
         }
 
         void LateUpdate() {
-            HandleZoom(Input.GetAxis("Mouse ScrollWheel"));
-            HandleDrag();
 
             if (ToTarget) {
                 TargetCharacter(ToTarget);
+                return;
+
             }
+            
+            if (zooming) return;
+            HandleZoom(Input.GetAxis("Mouse ScrollWheel"));
+            HandleDrag();
+
         }
 
         public void TargetCharacter(Character character) {
+            var zoomTime = 0f;
+            var scrollTime = 0f;
+
             var position = character.transform.position;
-            
+            zooming = true;
+            ToTarget = null;
+
             if (position.x == CameraPosition.x &&
-                position.y == CameraPosition.y) {
+                position.y == CameraPosition.y &&
+                position.z == CameraPosition.z) {
                     return;
             }
+            
+            var newPosition = new Vector3(position.x, position.y, position.z);
+            // transform.position = newPosition;
+            StartCoroutine(ChangeScrollOnSelect(transform, newPosition));
+            StartCoroutine(ChangeZoomOnSelect(MainCamera));
+            
 
-            ToTarget = character;
-            MainCamera.orthographicSize = 1.5f;
-            var newPosition = new Vector3(position.x, position.y, -10);
-            if(PointIsInsideBounds(ref newPosition, CameraBounds)) {
-                transform.position = newPosition;
+            IEnumerator ChangeScrollOnSelect(Transform transform, Vector3 finalPosition) {
+                while(transform.position != finalPosition) {
+                    scrollTime += Time.deltaTime / ScrollTime;
+                    transform.position = Vector3.Lerp(transform.position, finalPosition, scrollTime);
+                    yield return null;
+                }
+                yield return null;
+            }
+
+            IEnumerator ChangeZoomOnSelect(Camera camera) {
+                while(!camera.orthographicSize.Similar(2f, 0.0001f)) {
+                    zoomTime += Time.deltaTime / ZoomTime;
+                    camera.orthographicSize = Mathf.Lerp(camera.orthographicSize, 2f, zoomTime);
+                    yield return null;
+                }
+                zooming = false;
+                yield return null;
             }
         }
         
@@ -48,20 +85,20 @@ namespace Assets.Take_II.Scripts.GameManager {
             ToTarget = null;
             
             var change = CameraZoom + scrollMovement;
-            var newValue = Mathf.Clamp(change, 1, 4);
+            var newValue = Mathf.Clamp(change, 1, MaxZoom);
 
             MainCamera.orthographicSize = newValue;
             
             if (scrollMovement < 0) 
                 return;
 
-            if (newValue > 3.9) {
+            if (newValue > MaxZoom-0.9) {
                 NewPosition = CameraBounds.center;
                 transform.position = NewPosition;
                 return;
             }
             
-            var percentage = CameraZoom / 4f;
+            var percentage = CameraZoom / MaxZoom;
             NewPosition = Vector3.Lerp(CameraPosition, CameraBounds.center, 
                                 percentage / (CameraBounds.center - CameraPosition).magnitude);
             transform.position = NewPosition;
@@ -69,19 +106,23 @@ namespace Assets.Take_II.Scripts.GameManager {
        
         private void HandleDrag() {
             if (Input.GetMouseButtonDown (0)) {
-                MouseStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
                 return;
             }
             if (!Input.GetMouseButton (0)) {
                 return;
             }
-            MouseMove = new Vector2(Input.mousePosition.x - MouseStart.x, 
-                                    Input.mousePosition.y - MouseStart.y);
-            MouseStart = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            MouseMove = new Vector3(Input.mousePosition.x - MouseStart.x, 
+                                    Input.mousePosition.y - MouseStart.y,
+                                    Input.mousePosition.z - MouseStart.z);
+            MouseStart = new Vector3(Input.mousePosition.x, 
+                Input.mousePosition.y, 
+                Input.mousePosition.z);
 
-            var newX = transform.position.x + MouseMove.x * Time.deltaTime;
-            var newY = transform.position.y + MouseMove.y * Time.deltaTime;
-            NewPosition = new Vector3(newX, newY, -10);
+            var newX = transform.position.x + MouseMove.x * (Time.deltaTime / ScrollSpeed);
+            var newY = transform.position.y + MouseMove.y * (Time.deltaTime / ScrollSpeed);
+            var newZ = transform.position.z + MouseMove.z * (Time.deltaTime / ScrollSpeed);
+            NewPosition = new Vector3(newX, newY, newZ);
 
             if (PointIsInsideBounds(ref NewPosition, CameraBounds)) {
                 transform.position = NewPosition;
@@ -89,24 +130,24 @@ namespace Assets.Take_II.Scripts.GameManager {
         }
 
         private Bounds OrthographicBounds(Camera camera) {
-            float screenAspect = camera.aspect;
             float cameraHeight = CameraZoom * 2;
             return new Bounds( CameraPosition,
-                new Vector3(cameraHeight * screenAspect, cameraHeight, 0));
+                new Vector3(cameraHeight * camera.aspect, cameraHeight * camera.aspect, cameraHeight));
         }
 
         private bool PointIsInsideBounds(ref Vector3 point, Bounds bounds) {
             var extents = OrthographicBounds(MainCamera).extents;
             var left = point.x - extents.x > bounds.center.x - bounds.extents.x;
             var right = point.x + extents.x < bounds.center.x + bounds.extents.x;
+            
             var bottom = point.y - extents.y > bounds.center.y - bounds.extents.y;
             var top = point.y + extents.y < bounds.center.y + bounds.extents.y;
 
             if (!left || !right) {
-                point = new Vector3(CameraPosition.x, point.y, -10);
+                point = new Vector3(CameraPosition.x, point.y, CameraPosition.z);
             }
             if (!top || !bottom) {
-                point = new Vector3(point.x, CameraPosition.y, -10);
+                point = new Vector3(point.x, CameraPosition.y, CameraPosition.z);
             }
             
             return left || right || bottom || top;
