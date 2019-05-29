c72a7da3ef25fb2f6226099905d1c33fdfd9ead2
David Chavarria <dvchavarriam@gmail.com>
2019-05-28 18:12:40 -0600

    New Collection Extensions

---
+++ Assets/Utils/CollectionExtensions.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+namespace Assets.Utils {
+    public static partial class Extensions {
+        public static TValue GetValueOrDefault<TKey, TValue> (this IDictionary<TKey, TValue> dictionary,
+            TKey key, TValue defaultValue = default (TValue)) {
+            TValue value = defaultValue;
+            if (dictionary?.TryGetValue (key, out value) == true) {
+                return value;
+            }
+            return defaultValue;
+        }
+
+        public static void ForEach<T> (this IEnumerable<T> sequence, Action<T, int> action) {
+            var i = 0;
+            foreach (T item in sequence) {
+                action (item, i);
+                i++;
+            }
+        }
+
+        public static void RemoveNull<T> (this IList<T> sequence) where T : class {
+            for (var i = sequence.Count - 1; i >= 0; i--) {
+                if (sequence[i] == null)
+                    sequence.RemoveAt (i);
+            }
+        }
+    }
+}
