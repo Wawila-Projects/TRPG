6ee952aa5fde50b16c9fb2692938ed4f1f235311
David Chavarria <dvchavarriam@gmail.com>
2019-06-11 20:46:52 -0600

    Refactored new turn

--- Assets/Game System/TurnManager.cs
+++ Assets/Game System/TurnManager.cs
@@ -1,9 +1,9 @@
-﻿using UnityEngine;
-using Assets.CharacterSystem;
+﻿using Assets.CharacterSystem;
+using Asstes.CharacterSystem;
+using UnityEngine;
 
-namespace Assets.GameSystem
-{
-    public sealed class TurnManager: MonoBehaviour {
+namespace Assets.GameSystem {
+    public sealed class TurnManager : MonoBehaviour {
 
         public static TurnManager Manager { get; private set; }
 
@@ -15,49 +15,51 @@ namespace Assets.GameSystem
         public bool EnemyPhase => TurnCounter % 2 == 0;
         public bool Preround => TurnCounter == 0;
 
-        void Start()
-        {
+        void Start () {
             Manager = this;
             TurnCounter = 0;
-            NextTurn();
+            NextTurn ();
         }
 
-        public void ResetTurnCounter()
-        {
+        public void ResetTurnCounter () {
             TurnCounter = 1;
         }
 
-        public void StartRound()
-        {
+        public void StartRound () {
             TurnCounter = 1;
         }
 
-        public void NextTurn()
-        {
+        public void NextTurn () {
             ++TurnCounter;
-            Debug.Log($"Turn: {TurnCounter}");
-            
-            if (PlayerPhase)
-            {
+            Debug.Log ($"Turn: {TurnCounter}");
+
+            if (PlayerPhase) {
                 ++PlayerTurnCounter;
-                foreach (var player in GameController.Manager.Players)
-                {
-                    player.TurnFinished = false;
-                    player.ResetOneMore();
-                    player.CurrentMovement = player.Movement;
+                foreach (var player in GameController.Manager.Players) {
+                    NewTurn (player);
                 }
                 return;
             }
-            
+
             ++EnemyTurnCounter;
             // TODO: Add One More triggers to Enemy
-            foreach (var enemy in GameController.Manager.Enemies)
-            {
-                enemy.TurnFinished = false;
-                enemy.ResetOneMore();
-                enemy.CurrentMovement = enemy.Movement;
+            foreach (var enemy in GameController.Manager.Enemies) {
+                NewTurn (enemy);
             }
             return;
+
+            void NewTurn (Character character) {
+                character.ResetOneMore ();
+
+                if (character.StatusEffect == StatusConditions.Dizzy) {
+                    character.CurrentMovement = 0;
+                    character.TurnFinished = true;
+                    return;
+                }
+
+                character.TurnFinished = false;
+                character.CurrentMovement = character.Movement;
+            }
         }
     }
 }
