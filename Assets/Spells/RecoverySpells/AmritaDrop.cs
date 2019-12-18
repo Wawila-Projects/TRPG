8cca52ef5dab43696aa55705ea4a09a3281a4277
David Chavarria <dvchavarriam@gmail.com>
2019-12-18 14:53:47 -0600

    Fix Amrita spells to remove the correct spells

--- Assets/Spells/RecoverySpells/AmritaDrop.cs
+++ Assets/Spells/RecoverySpells/AmritaDrop.cs
@@ -1,18 +1,19 @@
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Utils;
 using Asstes.CharacterSystem.StatusEffects;
 
-
-namespace Assets.Spells.RecoverySpells
-{
-    public class AmritaDrop : RecoverySpell, IAssitSpell
-    {
+namespace Assets.Spells.RecoverySpells {
+    public class AmritaDrop : RecoverySpell, IAssitSpell {
         protected override string Id => "Assist7";
         public override string Name => "Amrita Drop";
         public override string Description => "Cure all ailments of 1 ally.";
         public override int Cost => 8;
         public override bool IsMultitarget => false;
-        public List<StatusCondition> CureableStatusConditions => EnumUtils<StatusCondition>.ToList();
+        public List<StatusCondition> CureableStatusConditions => EnumUtils<StatusCondition>.GetValues ().Where (
+            (sc) => sc != StatusCondition.None || sc != StatusCondition.Burn ||
+            sc != StatusCondition.Dizzy || sc != StatusCondition.Down ||
+            sc != StatusCondition.Shock || sc != StatusCondition.Freeze
+        ).ToList ();
     }
 }
--- Assets/Spells/RecoverySpells/AmritaShower.cs
+++ Assets/Spells/RecoverySpells/AmritaShower.cs
@@ -1,18 +1,20 @@
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using Assets.Utils;
 using Asstes.CharacterSystem.StatusEffects;
 
-
-namespace Assets.Spells.RecoverySpells
-{
-    public class AmritaShower : RecoverySpell, IAssitSpell
-    {
+namespace Assets.Spells.RecoverySpells {
+    public class AmritaShower : RecoverySpell, IAssitSpell {
         protected override string Id => "Assist8";
         public override string Name => "Amrita Shower";
         public override string Description => "Cure all ailments of party.";
         public override int Cost => 16;
         public override bool IsMultitarget => true;
-        public List<StatusCondition> CureableStatusConditions => EnumUtils<StatusCondition>.ToList();
+        public List<StatusCondition> CureableStatusConditions =>
+            EnumUtils<StatusCondition>.GetValues ().Where (
+                (sc) => sc != StatusCondition.None || sc != StatusCondition.Burn ||
+                sc != StatusCondition.Dizzy || sc != StatusCondition.Down ||
+                sc != StatusCondition.Shock || sc != StatusCondition.Freeze
+            ).ToList ();
     }
 }
